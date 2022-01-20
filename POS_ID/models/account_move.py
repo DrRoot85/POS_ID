3133b6c76e3e6325002b2d119a4f4cb9b6780713
Mahmoud Hassan Ahmed <dr.root85@gmail.com>
2022-01-20 21:08:09 +0300

    Last Change

--- POS_ID/models/account_move.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-
-class ResCompany(models.Model):
-    _inherit = 'account.move'
-
-    type = fields.Selection([('in_invoice', 'Lin_invoice'), ('in_refund', 'in_refund')], string='Type:')
